b73ade73413bc201a2449c50d93c7495c40fe0c2
TAsenova <tasenova@mentormate.com>
2024-11-04 14:53:46 +0200

    error handling middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,54 @@
+const { constants } = require("../constants");
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  switch (key) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Validation Failed",
+        statusCode,
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not Found",
+        statusCode,
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "Unauthorized",
+        statusCode,
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        statusCode,
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "Internal Server Error",
+        statusCode,
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    default:
+      console.log("No error, All good");
+      break;
+  }
+};
+
+module.exports = errorHandler;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Middleware
+
+app.use(express.json()); // Parser for client-side received data stream
+
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Server running on port " + port);
