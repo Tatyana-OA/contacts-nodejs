799711ef9ce620296107c01f8f15407b49c1cd96
TAsenova <tasenova@mentormate.com>
2024-11-04 18:31:37 +0200

    CRUD for db

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,19 +1,35 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 //@desc Get a particular contact
 //@route GET /api/contacts/:id
 //@access public
 
-const getContact = asyncHandler((req, res) => {
-  res.status(200).json({ message: "Get Contact " + req.params.id });
+const getContact = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const contact = await Contact.findById(id);
+
+  console.log(contact);
+
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  res.status(200).json({
+    message: "Successfully Retrieved Contact Id " + id,
+    contact,
+  });
 });
 
 //@desc Create new contact
@@ -27,7 +43,8 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are required");
   }
-  res.status(201).json({ message: "Create Contact" });
+  const contact = await Contact.create({ name, email, phone });
+  res.status(201).json(contact);
 });
 
 //@desc Edit existing contacts
@@ -35,7 +52,11 @@ const createContact = asyncHandler(async (req, res) => {
 //@access public
 
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update Contact " + req.params.id });
+  const { id } = req.params;
+  const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.status(200).json({ message: "Update Contact " + id, updatedContact });
 });
 
 //@desc Delete existing contacts
@@ -43,7 +64,14 @@ const updateContact = asyncHandler(async (req, res) => {
 //@access public
 
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Delete Contact " + req.params.id });
+  const { id } = req.params;
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  await contact.deleteOne();
+  res.status(200).json({ message: "Delete Contact " + id });
 });
 
 module.exports = {
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,7 +3,9 @@ const { constants } = require("../constants");
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
-  switch (key) {
+  console.log(statusCode);
+
+  switch (statusCode) {
     case constants.VALIDATION_ERROR:
       res.json({
         title: "Validation Failed",
