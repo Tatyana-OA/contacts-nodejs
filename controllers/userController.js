86e78a7d86765b552dc0fb38136421457044b536
TAsenova <tasenova@mentormate.com>
2024-11-07 19:14:22 +0200

    Register a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,33 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 //@desc Register a user
 //@route POST /users/register
 //@access public
 
-const registerUser = asyncHandler((req, res) => {
-  res.json({ message: "Register the user" });
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("Email already taken");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ username, email, password: hashedPassword });
+  if (user) {
+    res.status(201).json({
+      message: "User Registered",
+      email: user.email,
+      username: user.username,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
 });
 
 //@desc Login user
